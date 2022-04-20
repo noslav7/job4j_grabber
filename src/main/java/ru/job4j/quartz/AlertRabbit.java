935edb70e3405877d598e517fa923ba91ad0e154
noslav7 <noslav7@gmail.com>
2022-04-20 17:53:21 +0500

    1. Quartz [#175122]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,18 +2,26 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("./src/data/rabbit.properties");
+        int interval = getInterval(file);
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(interval)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -23,6 +31,16 @@ public class AlertRabbit {
         } catch (SchedulerException se) {
             se.printStackTrace();
         }
+    }
+
+    private static int getInterval(File file) {
+        int interval;
+        try (FileReader reader = new FileReader(file)) {
+            interval = reader.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return interval;
     }
 
     public static class Rabbit implements Job {
