b724c3336ea6ce794d07184f06377ae1cfd02bdd
noslav7 <noslav7@gmail.com>
2022-08-06 22:31:56 +0500

    4. Схема таблицы Post [#1731]

--- src/main/java/ru/job4j/aggregator/aggregator.sql
+++ src/main/java/ru/job4j/aggregator/aggregator.sql
@@ -1,9 +1,7 @@
-create schema agregator;
-
 create table post (
-                      id int PRIMARY KEY UNIQUE,
-                      name varchar(255),
-                      text text,
-                      link varchar(255) UNIQUE,
-                      created date
+id SERIAL PRIMARY KEY,
+name varchar(255),
+text text,
+link varchar(255) UNIQUE,
+created date
 );
