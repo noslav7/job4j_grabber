4568eb560972f811a8f26e6ee0685afc46881f73
noslav7 <noslav7@gmail.com>
2023-02-17 11:42:55 +0500

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,40 @@
+package ru.job4j.grabber;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        /* cnn = DriverManager.getConnection(...); */
+    }
+
+    @Override
+    public void save(Post post) {
+
+    }
+
+    @Override
+    public List<Post> getAll() {
+        return null;
+    }
+
+    @Override
+    public Post findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+}
