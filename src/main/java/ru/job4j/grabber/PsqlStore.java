de12fb559ee066acf6a26a10fd33919388c3b34f
noslav7 <noslav7@gmail.com>
2023-02-17 12:55:22 +0500

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -1,34 +1,85 @@
 package ru.job4j.grabber;
 
-import java.sql.Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-public class PsqlStore implements Store {
+public class PsqlStore implements Store, AutoCloseable {
+
+    public static final Logger LOG = LoggerFactory.getLogger(PsqlStore.class.getName());
     private Connection cnn;
 
-    public PsqlStore(Properties cfg) {
+    public PsqlStore(Properties cfg) throws SQLException {
         try {
             Class.forName(cfg.getProperty("jdbc.driver"));
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
-        /* cnn = DriverManager.getConnection(...); */
+        cnn = DriverManager.getConnection(
+                cfg.getProperty("url"),
+                cfg.getProperty("username"),
+                cfg.getProperty("password")
+        );
     }
 
     @Override
     public void save(Post post) {
-
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "insert into posts(name, text, link, created) values (?, ?, ?, ?);",
+                Statement.RETURN_GENERATED_KEYS
+        )) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+            try (ResultSet set = statement.getGeneratedKeys()) {
+                if (set.next()) {
+                    post.setId(set.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
     }
 
     @Override
     public List<Post> getAll() {
-        return null;
+        List<Post> rsl = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from posts;"
+        )) {
+            try (ResultSet set = statement.executeQuery()) {
+                while (set.next()) {
+                    rsl.add(getPost(set));
+                }
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return rsl;
     }
 
     @Override
     public Post findById(int id) {
-        return null;
+        Post rsl = null;
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from posts where id = ?;"
+        )) {
+            statement.setInt(1, id);
+            try (ResultSet set = statement.executeQuery()) {
+                while (set.next()) {
+                    rsl = getPost(set);
+                }
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return rsl;
     }
 
     @Override
@@ -36,5 +87,15 @@ public class PsqlStore implements Store {
         if (cnn != null) {
             cnn.close();
         }
+    }
+
+    private Post getPost(ResultSet set) throws SQLException {
+        return Post.of(
+                set.getInt("id"),
+                set.getString("name"),
+                set.getString("link"),
+                set.getString("text"),
+                set.getTimestamp("created").toLocalDateTime()
+        );
     }
 }
