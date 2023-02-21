c7e8898fe4aed52235f336ab1c000d7bf3bd8da3
noslav7 <noslav7@gmail.com>
2023-02-21 11:18:55 +0500

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/PsqlStore.java
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -2,8 +2,10 @@ package ru.job4j.grabber;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -13,6 +15,31 @@ public class PsqlStore implements Store, AutoCloseable {
     public static final Logger LOG = LoggerFactory.getLogger(PsqlStore.class.getName());
     private Connection cnn;
 
+    public static void main(String[] args) throws SQLException {
+        Post postJunior = new Post("junior java developer", "https://career.habr.com/vacancies/1000115597",
+                "responsibilities: task completion", LocalDateTime.parse("2023-2-19T15:30"));
+        Post postMiddle = new Post("middle java developer", "https://career.habr.com/vacancies/1000113405",
+                "responsibilities: project creation and execution", LocalDateTime.parse("2023-2-19T16:30"));
+        Post postSenior = new Post("senior java developer", "https://career.habr.com/vacancies/1000113428",
+                "responsibilities: problem solving", LocalDateTime.parse("2023-2-19T18:00"));
+
+
+        try (InputStream input = PsqlStore.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties properties = new Properties();
+            properties.load(input);
+
+            PsqlStore store = new PsqlStore(properties);
+            store.save(postJunior);
+            store.save(postMiddle);
+            store.save(postSenior);
+
+            System.out.println(store.getAll());
+            System.out.println(store.findById(0));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public PsqlStore(Properties cfg) throws SQLException {
         try {
             Class.forName(cfg.getProperty("jdbc.driver"));
@@ -21,7 +48,7 @@ public class PsqlStore implements Store, AutoCloseable {
         }
         cnn = DriverManager.getConnection(
                 cfg.getProperty("url"),
-                cfg.getProperty("username"),
+                cfg.getProperty("username_value"),
                 cfg.getProperty("password")
         );
     }
@@ -29,8 +56,8 @@ public class PsqlStore implements Store, AutoCloseable {
     @Override
     public void save(Post post) {
         try (PreparedStatement statement = cnn.prepareStatement(
-                "insert into post(name, text, link, created) values (?, ?, ?, ?)"
-                        + " ON CONFLICT (link) DO UPDATE SET link = excluded.link || ';' || post.link;",
+                "insert into posts(name, text, link, created) values (?, ?, ?, ?)"
+                        + " ON CONFLICT (link) DO UPDATE SET link = excluded.link || ';' || posts.link;",
                 Statement.RETURN_GENERATED_KEYS
         )) {
             statement.setString(1, post.getTitle());
@@ -52,7 +79,7 @@ public class PsqlStore implements Store, AutoCloseable {
     public List<Post> getAll() {
         List<Post> rsl = new ArrayList<>();
         try (PreparedStatement statement = cnn.prepareStatement(
-                "select * from post;"
+                "select * from posts;"
         )) {
             try (ResultSet set = statement.executeQuery()) {
                 while (set.next()) {
@@ -69,7 +96,7 @@ public class PsqlStore implements Store, AutoCloseable {
     public Post findById(int id) {
         Post rsl = null;
         try (PreparedStatement statement = cnn.prepareStatement(
-                "select * from post where id = ?;"
+                "select * from posts where id = ?;"
         )) {
             statement.setInt(1, id);
             try (ResultSet set = statement.executeQuery()) {
